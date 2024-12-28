a56f8d19d0719d5fc2e0ac6d8fe91a0167c84b2d
mitesh4000 <somone_from_hell@tutanota.com>
2024-12-28 17:28:19 +0530

    restart

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        abel: ['"Abel"', "sans-serif"],
+        teko: ['"Teko"', "sans-serif"],
+        "squada-one": ['"Squada One"', "sans-serif"],
+        mynereve: ['"Mynerve"', "sans-serif"],
+        "indie-flower": ['"Indie Flower"', "C059"],
+        "gloria-hallelujah": ['"Gloria Hallelujah"', "sans-serif"],
+        "saira-condensed": ['"Saira Condensed"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
